0b14a43b1406476f8099c6849961eed7c8f6662e
ronewa <ronewamaselesele200@gmail.com>
2025-11-10 21:01:55 +0200

    Implement delete functionality and update todo list display in CLI Todo App

--- CLI_Todo_List_App/TodoApp.py
+++ CLI_Todo_List_App/TodoApp.py
@@ -29,21 +29,43 @@ def readerTodoFile(filename='todo.txt'):
         print(f"No existing todo file found. Starting with an empty todo list.")
     return todoList
 
+def deleteTodoItem(todoList, itemIndex):
+    """Deletes a todo item from the list by its index.
 
+    Args:
+        todoList (list): The list of todo items.
+        itemIndex (int): The index of the item to delete.
+    """
+    if 0 <= itemIndex < len(todoList):
+        removedItem = todoList.pop(itemIndex)
+        print(f"Removed todo item: {removedItem}")
+    else:
+        print("Invalid index. No item removed.")
 
+def fileToArray(filename='todo.txt'):
+    """Reads todo items from a file and returns them as a list.
+
+    Args:
+        filename (str): The name of the file to read the todo items from.
+
+    Returns:
+        list: The list of todo items read from the file.
+    """
+    return readerTodoFile(filename)
 def main():
     print("Please enter your name:")
     user = input()
     print(f"Hello, {user}! Welcome to your Todo List App.")
-    todoList = []
+    todoList =[fileToArray()]
     while True:
         print("\nMenu:")
         print("1. Add Todo")
         print("2. View Todos")
         print("3. Save Todos to File")
-        print("4. Exit")
+        print("4. Delete a Item")
+        print("5. Exit")
         
-        choice = input("Choose an option (1-4): ")
+        choice = input("Choose an option (1-5): ")
         if choice == '1':
             todo = input("Enter a new todo item: ")
             todoList.append(todo)
@@ -55,6 +77,12 @@ def main():
         elif choice == '3':
             writerTodoFile(todoList)
         elif choice == '4':
+            print("Your Todo List:")
+            for idx, item in enumerate(todoList, start=1):
+                print(f"{idx}. {item}")
+            indexToDelete = int(input("Enter the number of the item to delete: ")) - 1
+            deleteTodoItem(todoList, indexToDelete)
+        elif choice == '5':
             print("Exiting the Todo List App. Goodbye!")
             break
         else:
